68fc1e81042cf611dbf7cacbe9071289af705971
ehdgm <ehdgms0539@naver.com>
2021-03-16 01:02:58 +0900

    index test

---
+++ src/main/java/com/rest/api/controller/HelloController.java
@@ -0,0 +1,38 @@
+package com.rest.api.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/helloworld")
+public class HelloController {
+
+    @GetMapping("/string")
+    @ResponseBody
+    public String helloWorldString(){
+        return "helloworld";
+    }
+
+    @GetMapping("/page")
+    public String helloWorld() {
+        return "helloworld";
+    }
+
+    @GetMapping("/json")
+    @ResponseBody
+    public Hello helloWorldJson() {
+        Hello hello = new Hello();
+        hello.message = "helloWorld";
+        return hello;
+    }
+
+    @Getter
+    @Setter
+    public static class Hello {
+        private String message;
+    }
+}
